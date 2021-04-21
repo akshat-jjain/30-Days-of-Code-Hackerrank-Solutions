a954e0c1e3be24a34ad27c3269961d789e6b6c15
AKSHAT JAIN <56213740+akshat-jjain@users.noreply.github.com>
2021-04-21 13:17:42 +0530

    30 Days of Code HackerRank Solutions Created

---
+++ Day 1: Data Types/1.py
@@ -0,0 +1,13 @@
+# Declare second integer, double, and String variables.
+
+# Read and save an integer, double, and String to your variables.
+x = int(input())
+y = float(input())
+z = str(input())
+# Print the sum of both integer variables on a new line.
+print(i+x)
+# Print the sum of the double variables on a new line.
+print(d+y)
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print(s+z)
