91cd5d6882abc59a02d0bae1a00d5807b4433398
AKSHAT JAIN <56213740+akshat-jjain@users.noreply.github.com>
2021-04-21 13:38:38 +0530

    30 Days of Code HackerRank Solutions Created

---
+++ Day 21: Generics/21.cpp
@@ -0,0 +1,15 @@
+
+
+/**
+*    Name: printArray
+*    Print each element of the generic vector on a new line. Do not return anything.
+*    @param A generic vector
+**/
+
+// Write your code here
+template<typename A>
+void printArray(vector<A> a)
+{
+    for(A i:a)
+    cout<<i<<endl;
+}
